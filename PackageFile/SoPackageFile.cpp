783ac70047c6b9e253025c82dbad2f793fe4946f
YouJianXi <oil@tghgame.com>
2013-07-14 02:34:25 +0800

    资源�

---
+++ PackageFile/SoPackageFile.cpp
@@ -0,0 +1,250 @@
+﻿//-----------------------------------------------------------------------------
+// SoPackageFile
+// (C) oil
+// 2013-07-13
+//-----------------------------------------------------------------------------
+#include "SoPackageFile.h"
+//-----------------------------------------------------------------------------
+namespace GGUI
+{
+	//-----------------------------------------------------------------------------
+	SoPackageFile::SoPackageFile()
+	:m_theFileMode(Mode_None)
+	,m_pFile(0)
+	,m_pSingleFileInfoList(0)
+	,m_nSingleFileInfoListCapacity(0)
+	,m_nSingleFileInfoListSize(0)
+	{
+
+	}
+	//-----------------------------------------------------------------------------
+	SoPackageFile::~SoPackageFile()
+	{
+		ReleasePackageFile();
+	}
+	//-----------------------------------------------------------------------------
+	SoPackageFile::OperationResult SoPackageFile::InitPackageFile(const char* pszPackageFile, FileMode theFileMode)
+	{
+		if (pszPackageFile == 0 //空指针
+			|| pszPackageFile[0] == 0 //空字符串
+			|| theFileMode == Mode_None) //无效的文件操作模式
+		{
+			return Result_InvalidParam;
+		}
+		if (m_pFile || m_theFileMode != Mode_None)
+		{
+			//资源包已经打开，必须先关闭才能打开下一个资源包。
+			return Result_PackageFileAlreadyOpen;
+		}
+		FILE* pFile = 0;
+		//记录是否需要解析资源包，即提取资源包已有的文件结构信息。
+		bool bParsePackageFile = false;
+		if (theFileMode == Mode_Read)
+		{
+			//读模式，资源包文件必须存在。
+			pFile = fopen(pszPackageFile, "rb");
+			if (pFile == 0)
+			{
+				//打开失败。
+				return Result_OpenFileFail;
+			}
+			else
+			{
+				//资源包存在，需要解析资源包。
+				bParsePackageFile = true;
+			}
+		}
+		else if (theFileMode == Mode_Write)
+		{
+			//写模式，如果资源包不存在则创建。
+			pFile = fopen(pszPackageFile, "r+");
+			if (pFile == 0)
+			{
+				//资源包不存在，则创建。
+				pFile = fopen(pszPackageFile, "w+");
+				if (pFile == 0)
+				{
+					return Result_CreateFileFail;
+				}
+				else
+				{
+					bParsePackageFile = false;
+				}
+			}
+			else
+			{
+				//资源包存在，需要解析资源包。
+				bParsePackageFile = true;
+			}
+		}
+		m_theFileMode = theFileMode;
+		m_pFile = pFile;
+		if (bParsePackageFile)
+		{
+			//解析资源包。
+			//未完待续
+		}
+		return Result_OK;
+	}
+	//-----------------------------------------------------------------------------
+	SoPackageFile::OperationResult SoPackageFile::ReleasePackageFile()
+	{
+		return Result_OK;
+	}
+	//-----------------------------------------------------------------------------
+	SoPackageFile::OperationResult SoPackageFile::Open(const char* pszFileName, stReadSingleFile& theFile)
+	{
+		return Result_OK;
+	}
+	//-----------------------------------------------------------------------------
+	SoPackageFile::OperationResult SoPackageFile::Close(stReadSingleFile& theFile)
+	{
+		return Result_OK;
+	}
+	//-----------------------------------------------------------------------------
+	SoPackageFile::OperationResult SoPackageFile::Read(void* pBuff, int nBuffSize, int nReadSize, int& nActuallyReadSize, stReadSingleFile& theFile)
+	{
+		return Result_OK;
+	}
+	//-----------------------------------------------------------------------------
+	SoPackageFile::OperationResult SoPackageFile::Tell(int& nFilePos, stReadSingleFile& theFile)
+	{
+		return Result_OK;
+	}
+	//-----------------------------------------------------------------------------
+	SoPackageFile::OperationResult SoPackageFile::Seek(int nOffset, SeekOrigin theOrigin, stReadSingleFile& theFile)
+	{
+		return Result_OK;
+	}
+	//-----------------------------------------------------------------------------
+	SoPackageFile::OperationResult SoPackageFile::InsertSingleFile()
+	{
+		return Result_OK;
+	}
+	//-----------------------------------------------------------------------------
+	SoPackageFile::OperationResult SoPackageFile::WritePackageHead()
+	{
+		return Result_OK;
+	}
+	//-----------------------------------------------------------------------------
+	SoPackageFile::OperationResult SoPackageFile::WriteSingleFile(FILE* pSingleFile)
+	{
+		return Result_OK;
+	}
+	//-----------------------------------------------------------------------------
+	SoPackageFile::OperationResult SoPackageFile::WriteAllSingleFileInfo()
+	{
+		return Result_OK;
+	}
+	//-----------------------------------------------------------------------------
+	SoPackageFile::OperationResult SoPackageFile::ParsePackageFile()
+	{
+		if (m_pFile == 0)
+		{
+			return Result_PackageFileHaveNotOpen;
+		}
+		int nResult = fseek(m_pFile, 0, SEEK_SET);
+		if (nResult != 0)
+		{
+			return Result_FileOperationError;
+		}
+		//读取文件头。
+		const size_t sizePackageHead = sizeof(m_stPackageHead);
+		size_t nActuallyReadSize = fread(&m_stPackageHead, sizePackageHead, sizePackageHead, m_pFile);
+		if (nActuallyReadSize != sizePackageHead)
+		{
+			//文件头没有读取完整。
+			return Result_FileOperationError;
+		}
+		//判断文件头。
+		bool bFileFlagOK = true;
+		const char* pszFileFlag = SoPackageFileFlag;
+		for (int i=0; i<SoPackageFileFlagLength; ++i)
+		{
+			if (m_stPackageHead.szFileFlag[i] != pszFileFlag[i])
+			{
+				bFileFlagOK = false;
+				break;
+			}
+		}
+		if (!bFileFlagOK)
+		{
+			return Result_IsNotPackageFile;
+		}
+		//获取SingleFile信息列表。
+		ReleaseSingleFileInfoList();
+		ReCreateSingleFileInfoList(m_stPackageHead.nFileCount);
+		if (m_pSingleFileInfoList == 0)
+		{
+			return Result_MemoryIsEmpty;
+		}
+		const size_t sizeSingleFileInfoList = m_stPackageHead.nFileCount * sizeof(stSingleFileInfo);
+		size_t nActuallyReadInfoListSize = fread(m_pSingleFileInfoList, sizeSingleFileInfoList, sizeSingleFileInfoList, m_pFile);
+		if (nActuallyReadInfoListSize != sizeSingleFileInfoList)
+		{
+			//SingleFile信息列表没有读取完整。
+			return Result_FileOperationError;
+		}
+		//SingleFile信息列表读取成功。
+		//生成map，未完待续
+		return Result_OK;
+	}
+	//-----------------------------------------------------------------------------
+	void SoPackageFile::ReCreateSingleFileInfoList(int nCapacity)
+	{
+		stSingleFileInfo* pSingleFileInfoList_Temp = m_pSingleFileInfoList;
+		//
+		m_pSingleFileInfoList = (stSingleFileInfo*)malloc(nCapacity * sizeof(stSingleFileInfo));
+		if (m_pSingleFileInfoList)
+		{
+			if (m_nSingleFileInfoListSize > 0)
+			{
+				memcpy(m_pSingleFileInfoList, pSingleFileInfoList_Temp, m_nSingleFileInfoListSize*sizeof(stSingleFileInfo));
+			}
+			m_nSingleFileInfoListCapacity = nCapacity;
+		}
+		else
+		{
+			//申请内存失败。
+			m_pSingleFileInfoList = 0;
+			m_nSingleFileInfoListCapacity = 0;
+			m_nSingleFileInfoListSize = 0;
+		}
+		//
+		if (pSingleFileInfoList_Temp)
+		{
+			free(pSingleFileInfoList_Temp);
+			pSingleFileInfoList_Temp = 0;
+		}
+	}
+	//-----------------------------------------------------------------------------
+	void SoPackageFile::ReleaseSingleFileInfoList()
+	{
+		m_nSingleFileInfoListCapacity = 0;
+		m_nSingleFileInfoListSize = 0;
+		if (m_pSingleFileInfoList)
+		{
+			free(m_pSingleFileInfoList);
+			m_pSingleFileInfoList = 0;
+		}
+	}
+	//-----------------------------------------------------------------------------
+	void SoPackageFile::FormatFileFullName(std::string& strOut, const char* pszIn)
+	{
+		strOut = pszIn;
+		int nLength = (int)strOut.length();
+		for (int i=0; i<nLength; ++i)
+		{
+			char& theC = strOut[i];
+			if (theC >= 'A' && theC <= 'Z')
+			{
+				theC += 32;
+			}
+			else if (theC == '\\')
+			{
+				theC = '/';
+			}
+		}
+	}
+}
+//-----------------------------------------------------------------------------
---
+++ PackageFile/SoPackageFile.h
@@ -0,0 +1,133 @@
+﻿//-----------------------------------------------------------------------------
+// SoPackageFile
+// (C) oil
+// 2013-07-13
+//-----------------------------------------------------------------------------
+#ifndef _SoPackageFile_h_
+#define _SoPackageFile_h_
+//-----------------------------------------------------------------------------
+#include <stdio.h>
+#include <string>
+#include <map>
+//-----------------------------------------------------------------------------
+#define SoPackageFileFlag "SOPACKAG"
+#define SoPackageFileFlagLength 8
+#define SoPackageFileVersion 1
+#define SoPackageFileMAX_PATH 260
+//-----------------------------------------------------------------------------
+namespace GGUI
+{
+	class SoPackageFile
+	{
+	public:
+		enum FileMode
+		{
+			Mode_None,
+			Mode_Read,
+			Mode_Write,
+		};
+		enum SeekOrigin
+		{
+			Seek_Set = 0,
+			Seek_Cur = 1,
+			Seek_End = 2,
+		};
+		enum OperationResult
+		{
+			Result_OK, //顺利完成
+			Result_InvalidParam, //存在无效的参数
+			Result_PackageFileHaveNotOpen, //资源包尚未打开，不能执行操作。
+			Result_PackageFileAlreadyOpen, //资源包已经打开，必须先关闭才能打开下一个资源包。
+			Result_IsNotPackageFile, //不是SoPackageFile文件
+			Result_OpenFileFail, //打开磁盘文件失败
+			Result_CreateFileFail, //创建磁盘文件失败
+			Result_FileOperationError, //C标准库中的文件操作函数返回了失败。
+			Result_MemoryIsEmpty, //申请内存失败，内存不足。
+		};
+		//资源包文件头。
+		struct stPackageHead
+		{
+			//文件标志。
+			char szFileFlag[SoPackageFileFlagLength];
+			//版本号。
+			int nVersion;
+			//一共有多少个SingleFile。
+			int nFileCount;
+			//SingleFile信息集合中，第一个stSingleFileInfo距离文件开始处的偏移量。
+			int nOffsetForFirstSingleFileInfo;
+		};
+		//SingleFile的信息。
+		struct stSingleFileInfo
+		{
+			//文件名
+			char szFileName[SoPackageFileMAX_PATH];
+			//文件原始大小
+			int nOriginalFileSize;
+			//文件在资源包内的大小（有可能经过了压缩）
+			int nEmbededFileSize;
+			//该文件距离资源包文件开始处的偏移量。
+			int nOffset;
+		};
+		struct stReadSingleFile
+		{
+			//资源包内每个文件都有一个文件ID。
+			int nFileID;
+			//文件原始大小。
+			int nFileSize;
+			//文件指针。
+			int nFilePointer;
+			//存储文件原始的完整内容。
+			char* pFileBuff;
+		};
+
+	public:
+		SoPackageFile();
+		~SoPackageFile();
+		OperationResult InitPackageFile(const char* pszPackageFile, FileMode theFileMode);
+		OperationResult ReleasePackageFile();
+
+		//<<<<<<<<<<<<<<<< 从资源包内读取一个文件 <<<<<<<<<<<<<<<<<<<<<<<<<
+		OperationResult Open(const char* pszFileName, stReadSingleFile& theFile);
+		OperationResult Close(stReadSingleFile& theFile);
+		OperationResult Read(void* pBuff, int nBuffSize, int nReadSize, int& nActuallyReadSize, stReadSingleFile& theFile);
+		OperationResult Tell(int& nFilePos, stReadSingleFile& theFile);
+		OperationResult Seek(int nOffset, SeekOrigin theOrigin, stReadSingleFile& theFile);
+		//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+		//<<<<<<<<<<<<<<<< 把一个磁盘文件写入资源包 <<<<<<<<<<<<<<<<<<<<<<<
+		OperationResult InsertSingleFile();
+		//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+	private:
+		//写入资源包文件头。
+		OperationResult WritePackageHead();
+		//把一个原始的SingleFile写入到资源包中。
+		OperationResult WriteSingleFile(FILE* pSingleFile);
+		//把stSingleFileInfo信息集合写入到资源包中。
+		OperationResult WriteAllSingleFileInfo();
+		//解析资源包，即提取资源包已有的文件结构信息。
+		OperationResult ParsePackageFile();
+
+		void ReCreateSingleFileInfoList(int nCapacity);
+		void ReleaseSingleFileInfoList();
+		//把文件名格式化成如下格式：
+		//1，把'\\'修改成'/'；
+		//2，把大写字母修改成小写字母；
+		void FormatFileFullName(std::string& strOut, const char* pszIn);
+
+	private:
+		FileMode m_theFileMode;
+		FILE* m_pFile;
+		//文件头。
+		stPackageHead m_stPackageHead;
+		//SingleFile信息列表。
+		stSingleFileInfo* m_pSingleFileInfoList;
+		//m_pSingleFileInfoList中可以容纳多少个stSingleFileInfo对象。
+		int m_nSingleFileInfoListCapacity;
+		//m_pSingleFileInfoList中有效stSingleFileInfo对象的个数。
+		int m_nSingleFileInfoListSize;
+	};
+}
+//-----------------------------------------------------------------------------
+#endif //_SoPackageFile_h_
+//-----------------------------------------------------------------------------
